a8f3ec8d739ec563d2f129538ce14d9d40cdf9f3
midocean <pritimakwana161198@gmail.com>
2023-02-06 16:17:22 +0530

    site update

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-	$('nav a[href*="#"]').on('click', function() {
-		$('html,body').animate({ screenTop: $($(this).attr('href')).offset().top - 100}, 2000);
-	});	
-});
-
-
